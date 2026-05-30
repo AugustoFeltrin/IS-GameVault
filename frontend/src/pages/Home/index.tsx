7b4a96ce335a3b161f3f6e90d1dc7d68e17f3c30
Vinicius Gaspar <viniciusgasparprf@gmail.com>
2026-05-30 17:46:41 -0300

    Criando o modelo inicial estático da HomePage e seus componentes

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,23 @@
+import NavBar from "../../components/NavBar";
+import Catalog from "../../components/Catalog";
+import FindYourGame from "../../components/FindYourGame";
+
 export default function Home() {
-    
     return (
-        <>
-            <main className="text-9xl">Home</main>
-        </>
+        <div className="flex flex-col min-h-screen">
+            <header className="sticky top-0 z-50 h-16 w-full">
+                <NavBar />
+            </header>
+
+            <main className="flex-1 flex flex-col gap-10 w-full max-w-7xl mx-auto px-6 py-10">
+                <div className="w-full h-24">
+                    <FindYourGame/>
+                </div>
+
+                <div className="w-full h-24 flex-1">
+                    <Catalog />
+                </div>
+            </main>
+        </div>
     );
 }
