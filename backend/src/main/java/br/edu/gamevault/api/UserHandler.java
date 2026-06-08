02ce1853c387a9228582dd25a717d8d4fd58a95f
Vinicius Gaspar <viniciusgasparprf@gmail.com>
2026-06-08 14:06:06 -0300

    Ajustando os Handlers

--- backend/src/main/java/br/edu/gamevault/api/UserHandler.java
+++ backend/src/main/java/br/edu/gamevault/api/UserHandler.java
@@ -13,7 +13,7 @@ import java.io.OutputStream;
 public class UserHandler implements HttpHandler {
 
     private final UserService userService;
-    private final ObjectMapper mapper = new ObjectMapper(); // O tradutor JSON!
+    private final ObjectMapper mapper = new ObjectMapper(); 
 
     public UserHandler(UserService userService) {
         this.userService = userService;
@@ -21,14 +21,12 @@ public class UserHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        // 1. CONFIGURAÇÃO DO CORS (Obrigatório para o React não chorar)
         exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
         exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
 
         String method = exchange.getRequestMethod();
 
-        // 2. O NAVEGADOR TESTANDO A CONEXÃO (Preflight)
         if (method.equalsIgnoreCase("OPTIONS")) {
             exchange.sendResponseHeaders(204, -1);
             return;
@@ -37,7 +35,6 @@ public class UserHandler implements HttpHandler {
         try {
             String path = exchange.getRequestURI().getPath();
 
-            // 3. ROTEAMENTO E PROCESSAMENTO
             if (method.equalsIgnoreCase("POST") && path.endsWith("/login")) {
                 handleLogin(exchange);
             } 
@@ -54,24 +51,19 @@ public class UserHandler implements HttpHandler {
     }
 
     private void handleRegister(HttpExchange exchange) throws IOException {
-        // Lê o JSON que veio do React
         InputStream is = exchange.getRequestBody();
         User incomingUser = mapper.readValue(is, User.class);
 
-        // Chama o Service
         User createdUser = userService.addUser(incomingUser);
 
-        // Devolve o JSON de sucesso
         String jsonResponse = mapper.writeValueAsString(createdUser);
         sendResponse(exchange, 201, jsonResponse);
     }
 
     private void handleLogin(HttpExchange exchange) throws IOException {
-        // Implementar leitura do email/senha e chamada do userService.login()...
-        // (Semelhante ao Register, mas validando credenciais)
+
     }
 
-    // Método utilitário para não repetir código de resposta
     private void sendResponse(HttpExchange exchange, int statusCode, String jsonResponse) throws IOException {
         exchange.getResponseHeaders().set("Content-Type", "application/json");
         byte[] responseBytes = jsonResponse.getBytes("UTF-8");
