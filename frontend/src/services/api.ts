97d59c347cd9f083fec0d4fc1a14e312346562dc
Vinicius Gaspar <viniciusgasparprf@gmail.com>
2026-06-08 15:26:15 -0300

    Adicionado o arquivo de api.ts para conexão com o frontend

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,39 @@
+const BASE_URL = 'http://localhost:8080';
+
+export const api = {
+  
+  register: async (name: string, email: string, password: string) => {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.erro || 'Erro ao realizar o cadastro. Tente novamente.');
+    }
+
+    return response.json();
+  },
+
+  login: async (email: string, password: string) => {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.erro || 'E-mail ou senha inválidos.');
+    }
+
+    return response.json();
+  }
+  
+};
