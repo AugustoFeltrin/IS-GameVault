d5999659b46636592f75b189ec6790435af56975
Vinicius Gaspar <viniciusgasparprf@gmail.com>
2026-06-08 16:31:05 -0300

    Adicionando componentes dinâmicos no banner de perfil conforme os dados do usuário

--- frontend/src/components/ProfileBanner/index.tsx
+++ frontend/src/components/ProfileBanner/index.tsx
@@ -1,14 +1,19 @@
 export default function ProfileBanner() {
+    const userName = localStorage.getItem("gamevault_user_name") || "Jogador";
+    const userEmail = localStorage.getItem("gamevault_user_email") || "email@indisponivel.com";
+
+    const userInitials = userName.substring(0, 2).toUpperCase();
+
     return (
         <div className="bg-surface border border-gray-500/20 px-8 h-full w-full rounded-3xl flex justify-between">
             <div className="flex gap-4 justify-center items-center">
                 <div className="bg-primary border-black border-3 h-22 w-22 rounded-full flex justify-center items-center">
-                    <span className="text-2xl font-bold">AS</span>
+                    <span className="text-2xl font-bold">{userInitials}</span>
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <span className="text-2xl font-bold">Alex Santos</span>
-                    <span className="text-sm text-gray-400">@alexsantos · Entrou em Jan 2026</span>
+                    <span className="text-2xl font-bold">{userName}</span>
+                    <span className="text-sm text-gray-400">{userEmail}</span>
                     <span className="text-sm text-gray-400">Último acesso há 2h</span>
                 </div>
             </div>
