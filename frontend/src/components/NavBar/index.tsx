c6a6e91aad2afaca5c64e1f685f520d8c260815f
Vinicius Gaspar <viniciusgasparprf@gmail.com>
2026-06-15 18:18:03 -0300

    Atualizando visual da NavBar e removendo resíduos

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -8,32 +8,44 @@ export default function NavBar() {
         localStorage.removeItem("gamevault_user_id");
         toast.success("Sessão encerrada. Até logo!");
         navigate("/login");
+
     };
+
+    const userName = localStorage.getItem("gamevault_user_name") || "Jogador";
+    const userInitials = userName.substring(0, 2).toUpperCase();
 
     return (
         <header className="h-full w-full bg-background/80 backdrop-blur-md border-b border-surface">
             <div className="max-w-7xl w-full h-full mx-auto px-6 flex justify-between items-center">
-                
-                <div className="flex gap-2 items-center">
-                    <div className="bg-primary h-8 w-8 rounded-full"></div>
-                    <span>GameVault</span>
-                </div>
+                <Link 
+                    to="/home" 
+                    className="flex items-center gap-2"
+                >
+                    <img 
+                        className="h-10 w-10" 
+                        src="src\assets\Logo.png"></img>
+                    <span><b className="text-primary">Game</b><b className="text-secondary">Vault</b></span>
+                </Link>
 
                 <nav className="flex items-center gap-8">
                     <Link to="/home" className="text-sm text-gray-300 hover:text-primary">Início</Link>
-                    <Link to="/home" className="text-sm text-gray-300 hover:text-primary">Descobrir</Link>
                     <Link to="/profile" className="text-sm text-gray-300 hover:text-primary">Meu Diário</Link>
                 </nav>
 
                 <div className="flex gap-4 items-center">
                     <button 
                         onClick={handleLogout} 
-                        className="text-sm text-gray-300 hover:text-primary"
+                        className="text-sm text-gray-300 hover:text-primary cursor-pointer"
                     >
                         Sair
                     </button>
 
-                    <div className="bg-surface h-10 w-10 rounded-full"></div>
+                    <Link 
+                        to="/profile" 
+                        className="bg-primary border border-black h-10 w-10 rounded-full flex justify-center items-center"
+                    >
+                        <span className="text-sm">{userInitials}</span>
+                    </Link>
                 </div>
                 
             </div>
